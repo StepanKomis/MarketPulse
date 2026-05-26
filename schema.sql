793127a019229914d6e74bae6b0cdffa49fa09a6
StepanKomis <komis.stepan@gmail.com>
2026-05-26 12:38:00 +0200

    Added separate table for symbols

--- schema.sql
+++ schema.sql
@@ -1,9 +1,15 @@
+CREATE TABLE Symbols (
+    id          BIGSERIAL PRIMARY KEY,
+    symbol      TEXT NOT NULL
+)
+
 CREATE TABLE prices (
     id          BIGSERIAL PRIMARY KEY,
     exchange    TEXT        NOT NULL,
-    symbol      TEXT        NOT NULL,
+    symbol      BIGINT      NOT NULL,
     price       NUMERIC(20, 8) NOT NULL,
     recorded_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    Foreign Key (symbol) REFERENCES (Symbols.id)
 );
 
 CREATE INDEX idx_prices_symbol_time ON prices (symbol, recorded_at DESC);
